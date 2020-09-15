8c35ece9c1c0d64735eaf21a81ca96b7ce91539b
Avijit Mandal <46240269+avijit1999@users.noreply.github.com>
2020-09-15 10:25:48 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:just_class/calling_constructor.dart';
+//import 'package:just_class/calling_constructor.dart';
 import 'package:just_class/tiles.dart';
 
 void main() {
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:tiles();
+      body: Tiles(),
     );
   }
 }
