1254a8c4ce9813425ecb9534a831aa0c8e59e7c6
Avijit Mandal <46240269+avijit1999@users.noreply.github.com>
2020-09-15 11:42:16 +0530

    Update tiles.dart

--- tiles.dart
+++ tiles.dart
@@ -2,37 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:just_class/calling_constructor.dart';
 //import 'package:http/http.dart';
 import 'package:dio/dio.dart';
-import 'dart:convert';
+//import 'dart:convert';
 
-class Tiles extends StatefullWidget{
+class Tiles extends StatefulWidget {
   @override
   _TilesState createState() => _TilesState();
 }
 
-  class _TilesState extends state<Tiles>{
+class _TilesState extends state<Tiles> {
+  getDioData() async {
+    var dio = Dio();
+    Response response =
+        await dio.get('https://jsonplaceholder.typicode.com/photos');
+    print(response.data[0]);
+  }
 
-  getDioData()  async {
-  var dio = Dio();
-  Response response = await dio.get('https://jsonplaceholder.typicode.com/photos');
-  print(response.data[0]);
-}
-    ConstList list =ConstList();
+  ConstList list = ConstList();
 
-    @override
-    Widget build(BuildContext context){
-      getDioData();
-      return Scaffold(
+  @override
+  // ignore: override_on_non_overriding_member
+  Widget build(BuildContext context) {
+    getDioData();
+    return Scaffold(
         body: ListView.builder(
-          itemCount:list.student.length,
-          itemBuilder:(BuildContext context,index)
-          {
-            return ListTitle(
-              title: Text('$(List.listName(index))'),
-              subtitle: Text('$(List.listRollNo(index))'),
-            );
-          }
-        )
-      );
-    }
+            itemCount: list.student.length,
+            itemBuilder: (BuildContext context, index) {
+              return ListTile(
+                title: Text('${list.listName(index)}'),
+                subtitle: Text('${list.listRollNo(index)}'),
+              );
+            }));
   }
 }
